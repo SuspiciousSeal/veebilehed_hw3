8c7377349a0a08b9179cc4f951dc03a7aa9230b9
LAPTOP-N55O18KP\Robert <robert.allik@protonmail.com>
2023-09-24 16:49:49 +0300

    added files

---
+++ footer.js
@@ -0,0 +1,19 @@
+//https://stackoverflow.com/a/15250208
+document.write(`
+<footer class="bg-light">
+            <div class="container-fluid">
+                <div class="row">
+                    <ul class="footer-links">
+                        <li><p>©: 23.09.2023</p></li>
+                        <li><p>tel: 55555555</p></li>
+                        <li><a href="mailto:email@email.com">@: email@email.com</p></li>
+                    </ul>
+                    
+                    <div class="footer-sm">
+                        <a href="#"><img src="instagram.png" alt="instagram" /></a>
+                        <a href="#"><img src="youtube.png" alt="youtube" /></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+`);
---
+++ header.js
@@ -0,0 +1,9 @@
+document.write(`
+<header class="py-2">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-12 my-3 text-center"><img src="logo2.png" width="50%"></div>
+      </div>
+  </div>
+</header>
+`);
